d581bc016371c7d2b86c8e7e170aa82e7608adbf
김원욱 <kimww0306@gmail.com>
2022-08-24 23:07:57 +0900

    로그인과 로그아웃 추가

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("core.urls"), name="core"),
+    path("users/", include("users.urls"), name="users"),
     path("admin/", admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView, FormView, CreateView
+from django.contrib.auth import login, logout
+from . import mixins, forms, models
 
 
-class UserHomeView(TemplateView):
+class UserHomeView(mixins.LoggedInOnlyView, TemplateView):
 
     """
     This is a home view for those who are logged in.
     """
 
     template_name = "users/home.html"
+
+
+class LoginView(mixins.LoggedOutOnlyView, FormView):
+
+    template_name = "users/login.html"
+
+    form_class = forms.LoginForm
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = models.User.objects.get(email=email)
+        if user.check_password(password):
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("users:login"))
+
+
+class SignUpView(mixins.LoggedOutOnlyView, CreateView):
+    
+    
+
+    pass
